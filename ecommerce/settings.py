76c6aad7983c15311dd5017e1f3de00cbe1f28ba
Mac <mac@Macs-MacBook-Pro.local>
2022-05-21 13:27:34 +0545

    done

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rjymuwj29)iww)z^t!ek!+up(74%=nv1vq=+fz^_*7r_yk4-#-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
