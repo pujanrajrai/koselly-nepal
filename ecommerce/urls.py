38341cbd2f65cb7dfbdabe5678508816180ca1b7
Pujan Raj Rai <70184988+pujanrajrai@users.noreply.github.com>
2023-02-11 22:41:53 +0545

    url update

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     # path('profile/', include('profiles.urls')),
     path('dashboard/', include('dashboard.urls')),
     # path('chat/', include('chat.urls')),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
