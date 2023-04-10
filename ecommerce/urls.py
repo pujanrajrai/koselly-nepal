85a8faf99c8f0239146ad2f46d1a136e2efbbc4d
pratik213 <dahalp30@gmail.com>
2023-04-10 19:02:19 +0545

    add to cart

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -21,9 +21,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('product/', include('products.urls')),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
     # path('accounts/', include('accounts.urls')),
-    path('', include('accounts.urls')),
+    path('account/', include('accounts.urls')),
     path('profile/', include('profiles.urls')),
     path('dashboard/', include('dashboard.urls')),
     # path('chat/', include('chat.urls')),
--- home/views.py
+++ home/views.py
@@ -28,7 +28,7 @@ def product_desc(request, pk):
     return render(request, 'home/product_desc.html', context)
 
 
-@is_admin_or_user()
+# @is_admin_or_user()
 def add_to_cart(request):
     if request.method == 'POST':
         quantity = int(request.POST['quantity'])
@@ -63,7 +63,7 @@ def remove_cart(request):
         return HttpResponse('Bad Request')
 
 
-@is_admin_or_user()
+# @is_admin_or_user()
 def show_cart(request):
     my_cart = Cart.objects.filter(user=request.user).filter(is_bought=False)
     price = 0
@@ -73,7 +73,7 @@ def show_cart(request):
     return render(request, 'home/cart.html', context)
 
 
-@is_admin_or_user()
+# @is_admin_or_user()
 def add_subtract_cart_item(request):
     if request.method == 'POST':
         cart = Cart.objects.filter(pk=request.POST['pk']).filter(user=request.user).get(is_bought=False)
@@ -96,7 +96,7 @@ def add_subtract_cart_item(request):
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
-@is_admin_or_user()
+# @is_admin_or_user()
 def checkout(request):
     if request.method == 'POST':
         uuid_str = str(uuid.uuid4().hex)
